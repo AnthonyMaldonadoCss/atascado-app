c8bcc78265432299e0d85057b93d9e4bcb7b6ef3
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-03-25 11:59:51 -0400

    feat: added routing to atascados app

---
+++ app/lib/routes/main.dart
@@ -0,0 +1,13 @@
+import 'package:atascados/widgets/pages/home_page.dart';
+import 'package:go_router/go_router.dart';
+
+class AppRouter {
+  GoRouter get router => _router;
+}
+
+final _router = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const HomePage(),
+  ),
+]);
