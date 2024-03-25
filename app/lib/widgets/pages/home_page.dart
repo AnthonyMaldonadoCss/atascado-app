8aeae8dfc0bf666665f0452baff1e322466a86e5
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-03-25 17:48:25 -0400

    feat: updated home widget

--- app/lib/widgets/pages/home_page.dart
+++ app/lib/widgets/pages/home_page.dart
@@ -10,6 +10,40 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Home Page')));
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(height: 10),
+        const Text(
+          'Atascados App',
+          style: TextStyle(fontSize: 40, fontFamily: 'Nexa'),
+        ),
+        const Image(
+          image: AssetImage('assets/images/parking.png'),
+          color: Colors.blueAccent,
+        ),
+        SizedBox(height: 50),
+        Row(mainAxisSize: MainAxisSize.min, children: [
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blueAccent,
+            ),
+            child: Row(
+              children: [
+                const Text('Logeate con ',
+                    style: TextStyle(color: Colors.white)),
+                const ImageIcon(
+                  AssetImage('assets/images/g-icon.png'),
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ])
+      ],
+    )));
   }
 }
