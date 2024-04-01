54685652faa5a7d18109c43e163d81217f728d3a
Anthony Maldonado <anthony@clay.cl>
2024-04-01 12:22:07 -0400

    configurarion files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TaskModule } from './task/task.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { BuildingsModule } from './buildings/buildings.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { NeighborsModule } from './neighbors/neighbors.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env.development'], isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.CONNECTION_STRING,
+      }),
+    }),
+    TaskModule,
+    UserModule,
+    BuildingsModule,
+    OrganizationsModule,
+    NeighborsModule,
+    VehiclesModule,
+  ],
 })
 export class AppModule {}
