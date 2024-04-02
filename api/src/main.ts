c227ddcb7cc92d20f59e01bbbd24247fc7d046b3
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-04-02 16:36:12 -0400

    feat: added swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Atascados API')
+    .setDescription('The Atascados API')
+    .setVersion('1.0')
+    .addTag('atascados')
+    .build();
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  Logger.log(
+    `Swagger docs running on http://localhost:${process.env.PORT}/docs`,
+    'Bootstrap',
+  );
   await app.listen(process.env.PORT);
+  Logger.log(
+    `Server running on http://localhost:${process.env.PORT}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
